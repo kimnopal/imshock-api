f339c17e6a68eb7c99ba109a0ba507a06860753f
kimnopal <naufalhakim366@gmail.com>
2024-03-18 00:32:22 +0700

    feat(db): connecting into database

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/kimnopal/imshock-api/app"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	db := app.NewDB()
+	err := db.Ping()
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Hello World")
+	}
 }
 
 // migrate -database "mysql://root@tcp(localhost:3306)/maya" -path db/migrations up
